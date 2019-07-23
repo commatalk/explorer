9a77a74d7641468a7a0e85bbff11243fd8f3e244
root <root@ip-172-31-17-152.ap-southeast-1.compute.internal>
2019-07-23 03:49:33 +0000

    remove nodemodule

--- explorer/node_modules/@dashevo/dashcore-lib/lib/govobject/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-// TODO: Remove previous line and work through linting issues at next edit
-
-module.exports = require('./govobject');
-
-module.exports.Proposal = require('./types/proposal');
